437e5c5c53238a7e01ce7a01915b79b5039f8656
Yihao <yihao.wang@scai.fraunhofer.de>
2023-03-01 12:18:15 +0100

    BERT training main code

--- TrainBERT.py
+++ TrainBERT.py
@@ -20,52 +20,45 @@ if __name__ == '__main__':
 
     bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-    # train_dir = "data/TBGA/TBGA_train_processed_subset.json"
-    # #train_dir = "data/DG_train.json"
-    # train_data, train_label, _, _ = process_txtdata(train_dir)
-    # training = list(zip(train_data, train_label))
+    train_dir = "dataset/TBGA/TBGA_train_processed.json"       #  | "dataset/DG/DG_train.json"
+    train_data, train_label, _, _ = process_txtdata(train_dir)
+    training = list(zip(train_data, train_label))
     #
-    # valid_dir = "data/TBGA/TBGA_val_processed.json"
-    # #valid_dir = "data/DG_valid.json"
-    # val_data, val_label, _, _ = process_txtdata(valid_dir)
-    # validation = list(zip(val_data, val_label))
+    valid_dir = "dataset/TBGA/TBGA_val_processed.json"         #  | "dataset/DG/DG_valid.json"
+    val_data, val_label, _, _ = process_txtdata(valid_dir)
+    validation = list(zip(val_data, val_label))
     #
-    # print("Process Data---------------------------------")
-    # train_dataset = TBGADataset(training, bert_tokenizer)
-    # print("Training set processed")
-    # valid_dataset = TBGADataset(validation, bert_tokenizer)
-    #
-    # print("Valid and Test set processed")
-    #
-    # train_dataloader = DataLoader(dataset=train_dataset, sampler=RandomSampler(train_dataset), batch_size=16,
-    #                               drop_last=True)
-    # validation_dataloader = DataLoader(dataset=valid_dataset, sampler=RandomSampler(valid_dataset), batch_size=16,
-    #                                    drop_last=True)
+    print("Process Data---------------------------------")
+    train_dataset = TBGADataset(training, bert_tokenizer)
+    print("Training set processed")
+    valid_dataset = TBGADataset(validation, bert_tokenizer)
+    print("Valid and Test set processed")
+
+    train_dataloader = DataLoader(dataset=train_dataset, sampler=RandomSampler(train_dataset), batch_size=16,
+                                   drop_last=True)
+    validation_dataloader = DataLoader(dataset=valid_dataset, sampler=RandomSampler(valid_dataset), batch_size=16,
+                                        drop_last=True)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    #
-    # # Training
+
+    ### Training
     model = MultiBert(cfg).to(device)
     optimizer = AdamW(model.parameters(), lr=1e-5, eps=1e-8)
 
 
     # epochs 2-4
-    # epochs = 3
-    #
-    # # Total number of training steps is [number of batches] x [number of epochs].
-    # # (Note that this is not the same as the number of training samples).
-    # total_steps = len(train_dataloader) * epochs
-    #
-    # # Create the learning rate scheduler.
-    # scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=100, num_training_steps=total_steps)
-    #
-    # trainigprocessBERT(epochs, model, optimizer, scheduler, train_dataloader, validation_dataloader, device)
+    epochs = 3
+
+    total_steps = len(train_dataloader) * epochs
+
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=100, num_training_steps=total_steps)
+
+    trainigprocessBERT(epochs, model, optimizer, scheduler, train_dataloader, validation_dataloader, device)
 
 
-    # EVAL----------------------------------------------
-    model, optimizer = load_model(model, optimizer, "./models/BERT_checkpoint_epoch_3.pth")
-    #test_dir = "data/TBGA/TBGA_test_processed.json"
-    test_dir = "data/DG_test.json"
+    ### Evaluation
+    #model, optimizer = load_model(model, optimizer, "./models/BERT_checkpoint_epoch_3.pth")
+    test_dir = "dataset/TBGA/TBGA_test_processed.json"     # | "dataset/DG/DG_test.json"
     test_data, test_label, _, _ = process_txtdata(test_dir)
     testing = list(zip(test_data, test_label))
     test_dataset = TBGADataset(testing, bert_tokenizer)
